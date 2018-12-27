6abebb1de19fa99fc78b1ccfdb3a636ebf9f2175
dunnkers <jeroenoverschie@gmail.com>
2018-12-27 18:48:17 +0100

    Updates

---
+++ precache-manifest.42cc757f5fd30941ea11be203858132b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1e9fbb651166448912d",
+    "url": "/static/js/main.b1e9fbb6.chunk.js"
+  },
+  {
+    "revision": "bbb4858fa7c28fa05cb9",
+    "url": "/static/js/1.bbb4858f.chunk.js"
+  },
+  {
+    "revision": "b1e9fbb651166448912d",
+    "url": "/static/css/main.b7f51ab7.chunk.css"
+  },
+  {
+    "revision": "85c5dbe86bbab46de6950a738878acfc",
+    "url": "/index.html"
+  }
+];
